ae6279f2764415c95bb24073450033e95cb6a03e
ibavli <ibrahim.bavli35@gmail.com>
2018-12-09 00:24:30 +0300

    Örnek exception

--- CustomException/Program.cs
+++ CustomException/Program.cs
@@ -15,33 +15,19 @@ namespace CustomException
         {
             Login("ali", "veli");
             Connect("https://www.testxxx.com");
+
+            Not(5);
+            Not(101);
+            Not(null);
+
+            BolmeIslemi("6", "3");
+            BolmeIslemi("6", "0");
+            BolmeIslemi("0", "5");
+            BolmeIslemi("xxx", "xxx");
+
             Console.Read();
         }
 
-        public class LoginException : Exception
-        {
-
-            public LoginException() : base()
-            {
-
-            }
-
-            public LoginException(String message) : base(message)
-            {
-
-            }
-
-            public LoginException(String message, Exception innerException) : base(message, innerException)
-            {
-
-            }
-
-            protected LoginException(SerializationInfo info, StreamingContext context) : base(info, context)
-            {
-
-            }
-
-        }
 
         public static void Login(string userName, string password)
         {
@@ -58,28 +44,6 @@ namespace CustomException
 
         }
 
-        public class ConnectException : Exception
-        {
-            public ConnectException() : base()
-            {
-
-            }
-
-            public ConnectException(String message) : base(message)
-            {
-
-            }
-
-            public ConnectException(String message, Exception innerException) : base(message, innerException)
-            {
-
-            }
-
-            protected ConnectException(SerializationInfo info, StreamingContext context) : base(info, context)
-            {
-
-            }
-        }
 
         public static void Connect(string url)
         {
@@ -110,5 +74,127 @@ namespace CustomException
                 Console.WriteLine(connectException.Message);
             }
         }
+
+        #region Örnek exception
+
+        public static void Not(byte? not)
+        {
+            try
+            {
+                if (not == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                byte _not = Convert.ToByte(not);
+
+                if (_not < 45)
+                {
+                    Console.WriteLine("Kaldınız");
+                }
+                else if (_not > 45 && _not < 100)
+                {
+                    Console.WriteLine("Geçtiniz");
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Değer sınır dışı");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format hatası");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Değer null olamaz");
+            }
+        }
+
+        public static void BolmeIslemi(string _sayi1, string _sayi2)
+        {
+            try
+            {
+                int sayi1 = Convert.ToInt32(_sayi1);
+                int sayi2 = Convert.ToInt32(_sayi2);
+                double sonuc = sayi1 / sayi2;
+                Console.WriteLine(sonuc.ToString());
+            }
+            catch (FormatException ex) 
+            {
+                Console.WriteLine("Lütfen doğru formatta veri giriniz.");
+            }
+            catch (OverflowException ex) //Değişken sınırları dışı
+            {
+                Console.WriteLine("Değişken sınırları dışına çıktınız.");
+            }
+            catch (DivideByZeroException ex) //Sayıları sıfıra bölmeye çalışırsa
+            {
+                Console.WriteLine("Sıfıra bölünme hatası. Sayı sıfıra bölünemez.");
+            }
+            catch (Exception ex) //Geri kalan tüm hataları denetler
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        #endregion
+
+        #region Login exception
+
+        public class LoginException : Exception
+        {
+
+            public LoginException() : base()
+            {
+
+            }
+
+            public LoginException(String message) : base(message)
+            {
+
+            }
+
+            public LoginException(String message, Exception innerException) : base(message, innerException)
+            {
+
+            }
+
+            protected LoginException(SerializationInfo info, StreamingContext context) : base(info, context)
+            {
+
+            }
+
+        }
+
+        #endregion
+
+        #region Connect exception
+
+        public class ConnectException : Exception
+        {
+            public ConnectException() : base()
+            {
+
+            }
+
+            public ConnectException(String message) : base(message)
+            {
+
+            }
+
+            public ConnectException(String message, Exception innerException) : base(message, innerException)
+            {
+
+            }
+
+            protected ConnectException(SerializationInfo info, StreamingContext context) : base(info, context)
+            {
+
+            }
+        }
+
+        #endregion
+
+
     }
 }
